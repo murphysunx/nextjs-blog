2c59d4a1cfb132997f7638480b2025a501eecff7
Murphy Sun <murphysunx@gmail.com>
2023-02-19 19:23:32 +1100

    #feat: nextjs setup

---
+++ components/date.js
@@ -0,0 +1,6 @@
+import { parseISO, form, format } from "date-fns";
+
+export default function Date({dateString}) {
+    const date = parseISO(dateString);
+    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+}
